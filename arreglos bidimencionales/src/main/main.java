105fd6f8512e9b4e707e3c100308b49140e3769c
jorge <jorgepadillagit@gmail.com>
2020-01-23 02:18:18 -0600

    se agrega proyecto arreglos bidimencionales

---
+++ arreglos bidimencionales/src/main/main.java
@@ -0,0 +1,38 @@
+package main;
+
+public class main {
+
+	public static void main(String[] args) {
+		// matrices
+		
+		
+		int matriz[][]=new int[4][3];
+		
+		matriz[0][0]=1;
+		matriz[0][1]=2;
+		matriz[0][2]=3;
+		
+		matriz[1][0]=4;
+		matriz[1][1]=5;
+		matriz[1][2]=6;
+
+		matriz[2][0]=7;
+		matriz[2][1]=8;
+		matriz[2][2]=9;
+		
+		matriz[3][0]=10;
+		matriz[3][1]=11;
+		matriz[3][2]=12;
+		
+		System.out.println(matriz[0][0]);
+		System.out.println(matriz[0][1]);
+		System.out.println(matriz[0][2]);
+		System.out.println(matriz[1][0]);
+		System.out.println(matriz[1][1]);
+		System.out.println(matriz[1][2]);
+		
+		
+		
+	}
+
+}
