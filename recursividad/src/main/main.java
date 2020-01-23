a45226d22525c48b6572d730ac105df3b5d3e9e0
jorge <jorgepadillagit@gmail.com>
2020-01-22 23:19:31 -0600

    se actualiza 22 02 2020

--- recursividad/src/main/main.java
+++ recursividad/src/main/main.java
@@ -4,6 +4,13 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int num=5, factorial;
+		if(num == 0){
+            return 1;
+        }
+        else
+            return num * factorial(num-1);
+		
 
 	}
 
